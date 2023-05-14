511f3086202b31d12abc460b5f4df707a958ef8f
matematik15 <pagowskipe@gmail.com>
2023-05-14 14:33:31 +0200

    clean up tests

--- src/test/java/com/ing/hackathon/atmservice/controller/AtmControllerTest.java
+++ src/test/java/com/ing/hackathon/atmservice/controller/AtmControllerTest.java
@@ -7,10 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
--- src/test/java/com/ing/hackathon/atmservice/service/AtmServiceTest.java
+++ src/test/java/com/ing/hackathon/atmservice/service/AtmServiceTest.java
@@ -3,12 +3,7 @@ package com.ing.hackathon.atmservice.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ing.hackathon.atmservice.model.Task;
-import com.ing.hackathon.transactions.model.Transaction;
-import com.ing.hackathon.transactions.service.DefaultTransactionService;
-import com.ing.hackathon.transactions.service.TransactionService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.io.InputStream;
 import java.util.List;
